102c3cc25f59dee16bb366cdf8cfbc5a13cc7c55
Maksym Diachuk <dia4uk.maksim@gmail.com>
2024-10-24 18:57:21 +0300

    Fixed calling api endpoints through aggregated swagger

--- reviewService/src/main/java/org/library/reviewService/config/OpenAPIConfig.java
+++ reviewService/src/main/java/org/library/reviewService/config/OpenAPIConfig.java
@@ -3,15 +3,25 @@ package org.library.reviewService.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
 
+    @Value("${server.port}")
+    private String port;
+
     @Bean
     public OpenAPI configureOpenAPI() {
+        Server server = new Server().url("http://localhost:" + port);
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info().title("Review API")
                         .description("API for Review Service")
                         .version("0.1")
